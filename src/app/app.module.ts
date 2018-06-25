8cc906ca3aa221ad655c1e668051781086654c4f
Mike <mike@8327.co.za>
2018-06-25 20:39:40 +0200

    Created the greeting component with basic style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { GreetingComponent } from './greeting/greeting.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    NavbarComponent
+    NavbarComponent,
+    GreetingComponent
   ],
   imports: [
     BrowserModule,
